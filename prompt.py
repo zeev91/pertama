dc47315c4dbd7d84141c855343c9bef935541481
zeev91 <richardo.simanullang@binus.ac.id>
2021-01-27 20:50:23 +0700

    python pertama

---
+++ prompt.py
@@ -0,0 +1,11 @@
+#this is first
+
+print('hello world')
+print('What is your name?')
+myName = input()
+print('it is good to meet you,' + myName)
+print('the length of your name is:')
+print(len(myName))
+print('what is your age?')
+myAge = input()
+print('you will be ' + str(int(myAge) + 1)+ ' in a year.')
